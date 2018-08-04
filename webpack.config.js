864d425cdd538f270efee67116b9f96c941dd7d3
Nicolas Blackburn <nicolas@nblackburn.ca>
2018-08-04 12:58:19 -0400

    Upgraded packages versions and updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,86 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
-const phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
-
 module.exports = {
+    mode: "development",
     entry: {
-      app: './src/app.ts'
+        app: "./src/app.ts"
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
         pathinfo: true
     },
     module: {
-        loaders: [
-            { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ },
-            { test: /pixi\.js/, loader: 'expose-loader?PIXI' },
-            { test: /phaser-split\.js$/, loader: ['expose-loader?Phaser'] },
-            { test: /p2\.js/, loader: ['expose-loader?p2'] }
+        rules: [
+            {
+                test: /pixi\.js$/,
+                use: [
+                    {
+                        loader: "expose-loader",
+                        options: "PIXI"
+                    }
+                ]
+            },
+            {
+                test: /phaser-split\.js$/,
+                use: [
+                    {
+                        loader: "expose-loader",
+                        options: "Phaser"
+                    }
+                ]
+            },
+            {
+                test: /p2\.js$/,
+                use: [
+                    {
+                        loader: "expose-loader",
+                        options: "p2"
+                    }
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ],
+                exclude: /node_modules/
+            }
         ]
     },
     resolve: {
-      extensions: [ ".tsx", ".ts", ".js" ],
-      modules: [
-        path.resolve(__dirname, 'src'),
-        path.resolve(__dirname, 'assets'),
-        'node_modules'
-      ],
-      alias: {
-        'phaser': path.join(phaserModule, 'build/custom/phaser-split.js'),
-        'pixi': path.join(phaserModule, 'build/custom/pixi.js'),
-        'p2': path.join(phaserModule, 'build/custom/p2.js')
-      }
+        extensions: [".tsx", ".ts", ".js"],
+        modules: [
+            path.resolve(__dirname, "src"),
+            path.resolve(__dirname, "assets"),
+            "node_modules"
+        ],
+        alias: {
+            phaser: path.join(__dirname, "node_modules/phaser-ce/build/custom/phaser-split.js"),
+            pixi: path.join(__dirname,  "node_modules/phaser-ce/build/custom/pixi.js"),
+            p2: path.join(__dirname,  "node_modules/phaser-ce/build/custom/p2.js")
+        }
     },
     plugins: [
-      new CopyWebpackPlugin([
-        {from: 'src/assets/www/*', to: './'},
-        {from: 'src/assets', to: 'assets', ignore: ['src/assets/www/*']}
-      ], {})
+        new CopyWebpackPlugin(
+            [
+                { from: "src/assets/www/*", to: "./" },
+                {
+                    from: "src/assets",
+                    to: "assets",
+                    ignore: ["src/assets/www/*"]
+                }
+            ],
+            {}
+        )
     ],
-    devtool: 'source-map',
+    devtool: "source-map",
     //devtool: 'cheap-module-eval-source-map',
     devServer: {
-      //host: "192.168.1.107",
-      contentBase: path.resolve(__dirname, 'dist')
+        //host: "192.168.1.107",
+        contentBase: path.resolve(__dirname, "dist")
     }
 };
