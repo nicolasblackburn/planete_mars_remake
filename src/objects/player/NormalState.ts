52fe3fa0e1ef87a70ae0bd633c825b9d94ed47eb
Nicolas Blackburn <nicolas@nblackburn.ca>
2018-08-05 14:21:22 -0400

    #11 Added minimal delay before changing animation state when moving

--- src/objects/player/NormalState.ts
+++ src/objects/player/NormalState.ts
@@ -1,48 +1,73 @@
 import { PlayerState } from './PlayerState';
 
 export class NormalState extends PlayerState {
-  public walkVelocity: number = 40;
-  protected bulletCount: number = 0;
-  
-  public hurt(damage: number) {
-    if (this.player.blinking) {
-      return;
+    public walkVelocity: number = 40;
+    protected bulletCount: number = 0;
+    protected previousMovementAnimationKey: string;
+    protected lastAnimationKeyChangeElapsedTime: number = -1
+
+    public hurt(damage: number) {
+        if (this.player.blinking) {
+            return;
+        }
+
+        this.player.health -= damage;
+        this.player.state.set('hurt');
     }
 
-    this.player.health -= damage;
-    this.player.state.set('hurt');
-  }
-  
-  public shoot() {
-    if (this.bulletCount < 3) {
-      this.bulletCount++;
-      const x = this.player.body.x;
-      const y = this.player.body.y;
-      const direction = this.player.inputHandler.direction;
-      const bullet = this.gameState.topRoom.addBullet(x, y, direction);
-      bullet.events.onKilled.addOnce(() => {
-        this.bulletCount--;
-      });
+    public shoot() {
+        if (this.bulletCount < 3) {
+            this.bulletCount++;
+            const x = this.player.body.x;
+            const y = this.player.body.y;
+            const direction = this.player.inputHandler.direction;
+            const bullet = this.gameState.topRoom.addBullet(x, y, direction);
+            bullet.events.onKilled.addOnce(() => {
+                this.bulletCount--;
+            });
+        }
     }
-  }
 
-  public update() {
-    const input = this.player.inputHandler;
-    const maxVelocity = this.walkVelocity;
-    const elapsedMS = this.game.time.elapsedMS * this.game.timeScale;
+    public update() {
+        const input = this.player.inputHandler;
+        const maxVelocity = this.walkVelocity;
+        const elapsedMS = this.game.time.elapsedMS * this.game.timeScale;
 
-    input.update();
+        input.update();
 
-    this.player.animations.play(this.player.animationKey(), null, true);
+        const animationKey = this.player.animationKey();
 
-    if (['move', 'move_shoot'].includes(input.state.key())) {
-      this.player.body.velocity.x = input.direction.x * maxVelocity * elapsedMS * input.directionScale;
-      this.player.body.velocity.y = input.direction.y * maxVelocity * elapsedMS * input.directionScale;
+        if (this.isMovementState(input.state.key())) {
 
-    } else {
-      this.player.body.velocity.x = 0;
-      this.player.body.velocity.y = 0;
+            if (this.lastAnimationKeyChangeElapsedTime < 0) {
+                this.previousMovementAnimationKey = animationKey;
+                this.player.animations.play(animationKey, null, true);
+                this.lastAnimationKeyChangeElapsedTime = 0;
+            } else {
+                this.lastAnimationKeyChangeElapsedTime += elapsedMS;
+                const minDelay = 100 * this.game.timeScale;
 
+                if (animationKey !== this.previousMovementAnimationKey && this.lastAnimationKeyChangeElapsedTime > minDelay) {
+                    this.previousMovementAnimationKey = animationKey;
+                    this.player.animations.play(animationKey, null, true);
+                    this.lastAnimationKeyChangeElapsedTime = 0;
+                }
+            }
+
+            this.player.body.velocity.x = input.direction.x * maxVelocity * elapsedMS * input.directionScale;
+            this.player.body.velocity.y = input.direction.y * maxVelocity * elapsedMS * input.directionScale;
+
+        } else {
+            this.lastAnimationKeyChangeElapsedTime = -1;
+            this.previousMovementAnimationKey = null;
+            this.player.animations.play(animationKey, null, true);
+            this.player.body.velocity.x = 0;
+            this.player.body.velocity.y = 0;
+
+        }
     }
-  }
+
+    protected isMovementState(state: string) {
+        return ['move', 'move_shoot'].includes(state);
+    }
 }
