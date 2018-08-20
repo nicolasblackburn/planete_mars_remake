34405ecb4821ebfc6a7e740706d66294b1323eae
Nicolas Blackburn <nicolas@nblackburn.ca>
2018-08-19 22:02:41 -0400

    Code format

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,18 +1,21 @@
 import { Game } from '../core/Game';
 
+/**
+ * This state preloads the initial game assets.
+ */
 export class Boot extends Phaser.State {
-  public init() {
-    const game = this.game as Game;
-    game.init();
-  }
+    public init() {
+        const game = this.game as Game;
+        game.init();
+    }
 
-  public preload() {
-    // Load assets for the loading screen
-    this.game.load.atlas('loading', 'assets/atlas/loading.png', 'assets/atlas/loading.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
-  }
+    public preload() {
+        // Load assets for the loading screen
+        this.game.load.atlas('loading', 'assets/atlas/loading.png', 'assets/atlas/loading.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    }
 
-  public create() {
-    const game = this.game as Game;
-		game.state.start('loading');
-  }
+    public create() {
+        const game = this.game as Game;
+        game.state.start('loading');
+    }
 }
